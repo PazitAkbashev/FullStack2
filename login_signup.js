6decd730c402b3121aa6bfe8d0adb9d1efe1ee8c
Pazit <116592094+PazitAkbashev@users.noreply.github.com>
2024-07-19 09:07:49 +0000

    create first page for login and signup

---
+++ login_signup.js
@@ -0,0 +1,9 @@
+document.getElementById('login').addEventListener('click', function() {
+    alert('Login page - coming soon');
+    // Here you can redirect the user to the login page
+});
+
+document.getElementById('signup').addEventListener('click', function() {
+    alert('Sign Up page - coming soon');
+    // Here you can redirect the user to the sign-up page
+});
