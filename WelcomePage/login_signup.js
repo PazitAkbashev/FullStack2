85baa6484e299f3b979394b2132cac6ed7a4aba9
Pazit1 <116592094+PazitAkbashev@users.noreply.github.com>
2024-07-19 14:56:14 +0300

    set up the fields and style of login/signup

--- WelcomePage/login_signup.js
+++ WelcomePage/login_signup.js
@@ -1,9 +1,11 @@
 document.getElementById('login').addEventListener('click', function() {
-    alert('Login page - coming soon');
+    //alert('Sign Up page - coming soon');
+    window.location.href ='./loginFiles/login.html';
     // Here you can redirect the user to the login page
 });
 
 document.getElementById('signup').addEventListener('click', function() {
-    alert('Sign Up page - coming soon');
+    window.location.href ='./signUpFiles/signUp.html';
+
     // Here you can redirect the user to the sign-up page
 });
